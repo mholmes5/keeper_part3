efb27b2a801ad860d73c4e16ac7f7efdfb1c669f
Michael Holmes <michaelholmes618@gmail.com>
2021-04-26 17:16:20 -0500

    Add delete

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 function Note(props) {
   return (
-    <div className="note">
+    <div
+      className="note"
+      onClick={() => {
+        props.onDelete(props.id);
+      }}
+    >
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <button>DELETE</button>
